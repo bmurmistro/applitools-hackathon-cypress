434d80640be4d84f06459e26fdaf7a27de04b385
Elad <elad.zohar@applitools.com>
2020-08-03 12:46:51 +0300

    add baseUrl

--- cypress/integration/examples/TraditionalSuite.js
+++ cypress/integration/examples/TraditionalSuite.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
 before(() => {
-  // cy.visit("https://demo.applitools.com/hackathon.html");
-  cy.visit("https://demo.applitools.com/hackathonV2.html");
+  cy.visit('hackathon.html');
+  // cy.visit('hackathonV2.html');
 });
 
 describe("Validate Labels", () => {
